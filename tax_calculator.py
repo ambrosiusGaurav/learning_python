a1609aef42297c0c624b2b21232ce2796cee40fe
Himal Pandey <pandu.himal@outlook.com>
2024-11-07 12:45:08 +1100

    Update tax_calculator.py
    Rename function name
    Return value

--- tax_calculator.py
+++ tax_calculator.py
@@ -1,9 +1,8 @@
-# calculate tax (default tax rate: 10%)
-def tax_calculator(income, tax=10):
-        net_income = income - income * tax / 100
-        print(net_income)
+# calculate and return net income (default tax rate: 10%)
+def net_income_calculator(income, tax=10):
+        return income - income * tax / 100
 
-tax_calculator(2500)
-tax_calculator(2500,20)
+print(net_income_calculator(2500))
+print(net_income_calculater(2500,20))
 
 
