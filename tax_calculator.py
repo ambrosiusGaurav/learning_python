46551d8621273e0c7bc23c3db9a78d76e1eb276e
Gaurav <redstargaurab5@gmail.com>
2024-11-07 12:13:37 +1100

    Refractored tax_calculator function, added tax parameter

--- tax_calculator.py
+++ tax_calculator.py
@@ -1,10 +1,10 @@
-def tax_calculator(income):
-        tax = 10
+def tax_calculator(income, tax=10):
+        
         net_income = income - income * tax / 100
         print(net_income)
 
 
 tax_calculator(2500)
-
+tax_calculator(2500,20)
 
 
